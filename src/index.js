78adef2e8341dc90d4bf01e75bcf0295501450d1
Shahar Yigal <shahar.yigal@sap.com>
2020-07-11 17:43:11 +0300

    s

--- src/index.js
+++ src/index.js
@@ -1,51 +1,14 @@
-// import React from 'react';
-// import ReactDOM from 'react-dom';
-// import { Provider } from 'react-redux';
-// import { BrowserRouter } from 'react-router-dom';
-// import { applyMiddleware, compose, createStore } from 'redux';
-// import thunk from 'redux-thunk';
-// import App from './Containers/App/App';
-// import './index.css';
-// import * as serviceWorker from './serviceWorker';
-// import reducer from './Store/reducers/app';
-//
-//
-// // Routes Configurations
-//
-// // redux configurations
-//
-//
-// const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-// const store = createStore( reducer, composeEnhancers( applyMiddleware( thunk ) ) );
-//
-// const app = (
-// 	<Provider store={store}>
-// 		<BrowserRouter>
-// 			<App name="roy"/>
-// 		</BrowserRouter>
-// 	</Provider>
-// )
-//
-// ReactDOM.render( app,
-// 	document.getElementById( 'root' )
-// );
-//
-//
-// // If you want your app to work offline and load faster, you can change
-// // unregister() to register() below. Note this comes with some pitfalls.
-// // Learn more about service workers: https://bit.ly/CRA-PWA
-// serviceWorker.unregister();
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
 import App from './App';
+import './index.css';
 import * as serviceWorker from './serviceWorker';
 
 ReactDOM.render(
 	<React.StrictMode>
-		<App />
+		<App/>
 	</React.StrictMode>,
-	document.getElementById('root')
+	document.getElementById( 'root' )
 );
 
 // If you want your app to work offline and load faster, you can change
