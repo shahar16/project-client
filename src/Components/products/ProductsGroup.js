76d2c040e63aba73aa1394eaff84fe0925135d0e
Shahar Yigal <shahar.yigal@sap.com>
2020-07-21 18:19:58 +0300

    Add ProductsGroup.js

---
+++ src/Components/products/ProductsGroup.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Col, Row } from "react-bootstrap";
+import ProductItem from "./ProductItem";
+
+function ProductsGroup( { items } ) {
+	return (
+		<div>
+			<br/>
+			<Row className="justify-content-md-center">
+				{items && items.map( item => <Col md={2}><ProductItem item={item}/></Col> )}
+			</Row>
+		</div>
+	);
+}
+
+export default ProductsGroup;
