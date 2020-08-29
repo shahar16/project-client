5e5a6efd194209dad5694453a4cab96dab3501ea
Shahar Yigal <shahar.yigal@sap.com>
2020-08-29 13:38:42 +0300

    Add link to ProductItem.js by route

--- src/Components/products/galleryView/ProductItem.js
+++ src/Components/products/galleryView/ProductItem.js
@@ -1,21 +1,27 @@
 import React from 'react';
-import { Badge, Card } from "react-bootstrap";
+import { Badge, Button, Card } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import constants from "../../../Shared/Util/Constants"
 
-function ProductItem({ item }) {
+function ProductItem({ item, renderStore }) {
 	return (
-		<Card style={{ width: '15rem', height: '20rem' }}>
-			<Card.Img variant="top" src={`${constants.serverUrl}/${item.image[0]}`} />
-			<Card.Body>
-				<div className={"d-flex mb-2 justify-content-between"} >
-					<Card.Title as={"h4"} className={"mb-0"}>{item.name}</Card.Title>
-					<Badge pill className={"mb-1"} variant={"warning"}>{item.price} ₪</Badge>
+		<Link to={`/products/${item.storeID}/${item.sn}`} >
+			<Card style={{ width: '15rem', height: '20rem' }}>
+				<div style={{height: '16rem'}}>
+					<Card.Img variant="top" src={`${constants.serverUrl}/${item.image[0]}`} />
 				</div>
-				{/*<div className={"d-flex mb-2 justify-content-between"}>*/}
-				{/*	<Button variant="primary" className={"mt-auto"} block>Go somewhere</Button>*/}
-				{/*</div>*/}
-			</Card.Body>
-		</Card>
+				<Card.Body>
+					<div className={"d-flex mb-0 justify-content-between"} >
+						<Card.Title as={"h4"} className={"mb-0"}>{item.name}</Card.Title>
+						<Badge pill className={"mb-1"} variant={"warning"}>{item.price} ₪</Badge>
+					</div>
+					{/*TODO: add delete button*/}
+					{/*<div className={"d-flex mb-2 justify-content-between"}>*/}
+					{/*	<Button variant="primary" className={"mt-auto"} block>Go somewhere</Button>*/}
+					{/*</div>*/}
+				</Card.Body>
+			</Card>
+		</Link>
 	)
 }
 
