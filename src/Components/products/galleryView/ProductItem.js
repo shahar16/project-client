6ee959a480dba679965c5cac3af9d97d0a69a190
Shahar Yigal <shahar.yigal@sap.com>
2020-08-29 14:48:09 +0300

    Add deleteProduct button to ProductItem.js (for now, only depends on renderStore)

--- src/Components/products/galleryView/ProductItem.js
+++ src/Components/products/galleryView/ProductItem.js
@@ -2,11 +2,15 @@ import React from 'react';
 import { Badge, Button, Card } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import constants from "../../../Shared/Util/Constants"
+import DeleteProduct from "../deleteProduct";
 
 function ProductItem({ item, renderStore }) {
+	const styleWithoutDelete = { width: '15rem', height: '20rem' };
+	const styleWithDelete = { width: '15rem', height: '24rem' };
+
 	return (
-		<Link to={`/products/${item.storeID}/${item.sn}`} >
-			<Card style={{ width: '15rem', height: '20rem' }}>
+		<Card style={renderStore ? styleWithDelete : styleWithoutDelete}>
+			<Link to={`/products/${item.storeID}/${item.sn}`} >
 				<div style={{height: '16rem'}}>
 					<Card.Img variant="top" src={`${constants.serverUrl}/${item.image[0]}`} />
 				</div>
@@ -15,13 +19,15 @@ function ProductItem({ item, renderStore }) {
 						<Card.Title as={"h4"} className={"mb-0"}>{item.name}</Card.Title>
 						<Badge pill className={"mb-1"} variant={"warning"}>{item.price} ₪</Badge>
 					</div>
-					{/*TODO: add delete button*/}
-					{/*<div className={"d-flex mb-2 justify-content-between"}>*/}
-					{/*	<Button variant="primary" className={"mt-auto"} block>Go somewhere</Button>*/}
-					{/*</div>*/}
 				</Card.Body>
-			</Card>
-		</Link>
+			</Link>
+			{renderStore && <Card.Footer>
+				{/*TODO: add delete button*/}
+				<div className={"d-flex mb-0 mt-0 justify-content-between"}>
+					<DeleteProduct product={item}/>
+				</div>
+			</Card.Footer>}
+		</Card>
 	)
 }
 
