e47923c32703fcbc37b56cad209644491bc4914d
Shahar Yigal <54366111+shahar16@users.noreply.github.com>
2020-08-30 22:02:22 +0300

    Add search functionality (#17)

--- src/Containers/HomePage.js
+++ src/Containers/HomePage.js
@@ -1,70 +1,72 @@
 import React, { useEffect } from 'react';
 import { connect } from "react-redux";
-import { BrowserRouter, Redirect, Route, Switch } from "react-router-dom";
-import Error from "../Components/Error";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 import ModalForAuth from "../Components/modals/ModalForAuth";
-import ModalForNewProduct from "../Components/modals/ModalForNewProduct";
 import NavBar from "../Components/NavBar";
+import ProductsGalleryView from "../Components/products/galleryView/ProductsGalleryView";
+import SearchTow from "../Components/SearchBox";
+import ProductService from "../Services/product.test.service";
 import * as actions from "../Store/actions";
 import ProductPage from "./ProductPage";
-import fakeProduct from "../Shared/Util/fakeProduct";
-import fakeStores from "../Shared/Util/fakeStores";
-import StorePage from './StorePage';
-import ProductsGalleryView from "../Components/products/galleryView/ProductsGalleryView";
-import ProductService from "../Services/product.test.service";
+import SearchResults from "./SearchResults";
 import StoreManagementPage from "./StoreManagementPage";
+import StorePage from './StorePage';
 
-function HomePage(props) {
+function HomePage( props ) {
 
-	useEffect(() => {
+	useEffect( () => {
 		props.checkAuth();
-	}, [props]);
+	}, [ props ] );
 
-	const getHomePageProducts = async (init) => {
-		return await ProductService.getHomePageProducts(init);
+	const getHomePageProducts = async ( init ) => {
+		return await ProductService.getHomePageProducts( init );
 	}
 
 	return (
 		<BrowserRouter>
-			<NavBar />
+			<Route component={NavBar} />
 			<div className="under-nav-bar">
 				<Switch>
 					{/*http://localhost:3000/login*/}
 					<Route exact
-						path="/"
-						render={(props) => (
-							<ProductsGalleryView {...props} fetchService={getHomePageProducts} />
-						)}
+						   path="/"
+						   render={( props ) => (
+							   <ProductsGalleryView {...props} fetchService={getHomePageProducts}/>
+						   )}
 					/>
 					<Route exact
-						path="/login"
-						render={(props) => (
-							<ModalForAuth {...props} signUp={false} />
-						)}
+						   path="/login"
+						   render={( props ) => (
+							   <ModalForAuth {...props} signUp={false}/>
+						   )}
 					/>
 					{/*http://localhost:3000/signUp*/}
 					<Route exact
-						path="/signup"
-						render={(props) => (
-							<ModalForAuth {...props} signUp={true} />
-						)}
+						   path="/signup"
+						   render={( props ) => (
+							   <ModalForAuth {...props} signUp={true}/>
+						   )}
 					/>
 					{/*http://localhost:3000/products/e3725de0-bd33-4d2a-a05e-dd6c7cbd5601/123456*/}
 					<Route exact
-						path="/products/:storeID/:sn"
-						component={ProductPage}
+						   path="/products/:storeID/:sn"
+						   component={ProductPage}
 					/>
 					{/*http://localhost:3000/my-stores*/}
 					<Route exact
-						path="/my-stores"
-						component={StoreManagementPage}
+						   path="/my-stores"
+						   component={StoreManagementPage}
 					/>
 					{/*TODO: Should be the lat path!!!*/}
 					{/* <Route path="*" component={Error} /> */}
-				http://localhost:3000/stores/e3725de0-bd33-4d2a-a05e-dd6c7cbd5601
-				<Route exact
-						path="/stores/:storeID"
-						component={StorePage}
+					{/*http://localhost:3000/stores/e3725de0-bd33-4d2a-a05e-dd6c7cbd5601*/}
+					<Route exact
+						   path="/stores/:storeID"
+						   component={StorePage}
+					/>
+					<Route exact
+						   path="/search/:query"
+						   component={SearchResults}
 					/>
 				</Switch>
 
@@ -77,10 +79,10 @@ function HomePage(props) {
 	);
 }
 
-const mapDispatchToProps = (dispatch) => {
+const mapDispatchToProps = ( dispatch ) => {
 	return {
-		checkAuth: () => dispatch(actions.checkState())
+		checkAuth: () => dispatch( actions.checkState() )
 	}
 };
 
-export default connect(null, mapDispatchToProps)(HomePage);
+export default connect( null, mapDispatchToProps )( HomePage );
