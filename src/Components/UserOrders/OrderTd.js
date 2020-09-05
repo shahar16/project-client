4f3a83dfbc067684c466984d137a209b61af6d44
ahDaniel <danielahrak@gmail.com>
2020-09-05 18:52:49 +0300

    UserOrdersPage

---
+++ src/Components/UserOrders/OrderTd.js
@@ -0,0 +1,39 @@
+import React from 'react';
+import { Image } from "react-bootstrap";
+import { Link } from "react-router-dom";
+import Constants from "../../Shared/Util/Constants";
+
+function OrderTd({ order, index }) {
+    return (
+        <tr>
+            <td>
+                {index + 1}
+            </td>
+            <td>
+                <Image
+                    className="d-block w-100"
+                    src={`${Constants.serverUrl}/${order.products[0].image[0]}`}
+                    alt={order.id}
+                    style={{ maxWidth: "100px", maxHeight: "100px" }}
+                />
+            </td>
+            <td>
+                <Link to={`/orders/${order.id}`}>
+                    {order.id}
+                </Link>
+                {order.id}
+            </td>
+            <td>
+                {order.totalSum + " ILS"}
+            </td>
+            <td>
+                {order.orderDate.date}
+            </td>
+            <td>
+                {}
+            </td>
+        </tr>
+    )
+}
+
+export default OrderTd;
